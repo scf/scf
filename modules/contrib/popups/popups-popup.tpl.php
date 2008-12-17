79b1c42bae9a4989f77487941b3e8adc4fd457c2
Benjamin Melancon <ben@agaricdesign.com>
2008-12-17 10:23:04 -0500

    popups, popups reference, and hovertips andded to SCF to meet pdonline needs

---
+++ modules/contrib/popups/popups-popup.tpl.php
@@ -0,0 +1,16 @@
+<?php
+// $Id: popups-popup.tpl.php,v 1.1.2.5 2008/11/04 00:39:48 starbow Exp $
+/**
+ * @file
+ */
+?>
+<div id="popups">
+  <div id="popups-title">
+    <div id="popups-close"><a href="#"><?php print t('Close') ?></a></div>
+    <div class="title">%title</div>
+    <div class="clear-block"></div>
+  </div>
+  <div id="popups-body">%body</div>
+  <div id="popups-buttons">%buttons</div>
+  <div id="popups-footer"></div>
+</div>
